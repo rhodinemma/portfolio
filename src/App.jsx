7c975f9c87de009b849de654db6da823792cb693
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-02-19 02:53:41 +0300

    add files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss'
+import {About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from "./components";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
