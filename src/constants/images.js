814746b4ded9905de3f679e3024352140eb799d7
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-02-19 03:47:38 +0300

    update logo and profile photo

--- src/constants/images.js
+++ src/constants/images.js
@@ -17,12 +17,14 @@ import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
+import dove from '../assets/dove.png';
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 
+import rhodo from '../assets/rhodo.png';
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
@@ -69,4 +71,6 @@ export default {
   nb,
   skype,
   spotify,
+  rhodo,
+  dove
 };
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__header-img"
       >
-        <img src={images.profile} alt="profile_bg" />
+        <img src={images.rhodo} alt="profile_bg" />
 
         <motion.img
           whileInView={{ scale: [0, 1] }}
